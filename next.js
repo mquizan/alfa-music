cbea710cea9eb4b24f8aa9f2dddca487ce1c9f11
mquizan <113592593+mquizan@users.noreply.github.com>
2023-11-06 14:32:23 +0300

    Add files via upload

---
+++ next.js
@@ -0,0 +1,100 @@
+const brandData = document.getElementById('brand-data').innerHTML + '/';
+
+const audioPlayer = document.getElementById('au-player');
+const nextButton = document.getElementById('nextButton');
+const musicFolderPath = './music/' + brandData;
+let musicFiles = [];
+let currentIndex = 0;
+ fetchMusicFiles();
+
+function fetchMusicFiles() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', musicFolderPath, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const parser = new DOMParser();
+      const htmlDocument = parser.parseFromString(xhr.responseText, 'text/html');
+      const links = htmlDocument.getElementsByTagName('a');
+      musicFiles = Array.from(links)
+        .map((link) => link.getAttribute('href'))
+        .filter((href) => href.endsWith('.mp3'));
+      const storedIndex = localStorage.getItem('currentIndex');
+      if (storedIndex) {
+        currentIndex = parseInt(storedIndex);
+      }
+      playMusicAtIndex(currentIndex);
+    }
+  };
+  xhr.send();
+}
+
+
+function playMusicAtIndex(index) {
+  const musicFile = musicFiles[index];
+  audioPlayer.src =  musicFolderPath + musicFile;
+  audioPlayer.load(); // Preload the audio file
+            //  document.addEventListener('click', function() {
+            //    audioPlayer.pause();
+            //  });
+  currentIndex = index;
+  localStorage.setItem('currentIndex', currentIndex.toString());
+  const musicTitleElement = document.getElementById('musicTitle');
+                      musicTitleElement.textContent = musicFile.replace(/%20/g, " ").replace(/\//g, "").replace(/FireGrill/g, "").replace(/Piatto/g, "").replace(/SteakHouse/g, "").replace(/music/g, "").replace(/X2Download.app/g, "");
+  // Preload the next 10 music files
+  for (let i = 1; i <= 10; i++) {
+    const nextIndex = (currentIndex + i) % musicFiles.length;
+    const nextMusicFile = musicFiles[nextIndex];
+    const nextAudio = new Audio();
+    nextAudio.src =  musicFolderPath + nextMusicFile;
+    nextAudio.load();
+
+    // Check if the nextMusicFile is already in the disk cache
+    if ('caches' in window) {
+      caches.match(nextMusicFile).then(response => {
+        if (!response) {
+          localStorage.setItem(`nextSong${i}`, nextMusicFile);
+        }
+      });
+    } else {
+      localStorage.setItem(`nextSong${i}`, nextMusicFile);
+    }
+  }
+}
+audioPlayer.addEventListener('error', ()=>{
+    nextButton.click();
+}
+);
+// function playMusicAtIndex(index) {
+//  const musicFile = musicFiles[index];
+//  audioPlayer.src = musicFolderPath + musicFile;
+//  document.addEventListener('click', function() {
+//  audioPlayer.play();
+//});
+//  currentIndex = index;
+   // Store the current index in localStorage
+//  localStorage.setItem('currentIndex', currentIndex.toString());
+   // Update the music title element
+//  const musicTitleElement = document.getElementById('musicTitle');
+//  musicTitleElement.textContent = musicFile.replace(/%20/g, " ");
+//}
+ audioPlayer.addEventListener('ended', () => {
+  const nextIndex = (currentIndex + 1) % musicFiles.length;
+  playMusicAtIndex(nextIndex);
+});
+ nextButton.addEventListener('click', () => {
+  const nextIndex = (currentIndex + 1) % musicFiles.length;
+  playMusicAtIndex(nextIndex);
+});
+// window.addEventListener('beforeunload', () => {
+//  // Remove the stored index from localStorage
+//  localStorage.removeItem('currentIndex');
+//});
+
+const randomButton = document.getElementById('randomButton');
+ randomButton.addEventListener('click', () => {
+  const randomIndex = Math.floor(Math.random() * musicFiles.length);
+  playMusicAtIndex(randomIndex);
+});
+
+audioPlayer.controls = true;
+audioPlayer.controlsList.add('nodownload');
---
+++ prayer.js
@@ -0,0 +1,145 @@
+// Set the API endpoint and parameters
+const cityData = document.getElementById('city-data').innerHTML;
+const url = 'https://api.aladhan.com/v1/timingsByCity';
+const params = {
+    city: cityData,
+    country: 'KSA',
+    method: 4,
+    school: 0,
+
+};
+// Set the API headers
+const http = 'http';
+const headers = ((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://api.aladhan.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+});
+
+function fetchPrayerTimes() {
+    // Fetch the prayer times from the API
+    fetch(`${url}?${new URLSearchParams(params)}`, { headers })
+        .then(response => response.json())
+        .then(data => {
+            // Extract the prayer times for today
+            const prayerTimes = data.data.timings;
+            const today = new Date().toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' });
+            const times = [
+                { prayer: 'Fajr', time: prayerTimes.Fajr },
+                { prayer: 'Sunrise', time: prayerTimes.Sunrise },
+                { prayer: 'Dhuhr', time: prayerTimes.Dhuhr },
+                { prayer: 'Asr', time: prayerTimes.Asr },
+                { prayer: 'Maghrib', time: prayerTimes.Maghrib },
+                { prayer: 'Isha', time: prayerTimes.Isha }
+            ];
+            // Display the prayer times on the webpage
+            const prayerTimesElement = document.getElementById('prayer-times');
+            prayerTimesElement.innerHTML = ''; // Clear previous prayer times
+            times.forEach(time => {
+                const row = document.createElement('tr');
+                const prayerCell = document.createElement('td');
+                prayerCell.innerText = time.prayer;
+                row.appendChild(prayerCell);
+                const timeCell = document.createElement('td');
+                let count = 1;
+                timeCell.classList.add('pTime');
+                timeCell.innerText = time.time;
+                row.appendChild(timeCell);
+                prayerTimesElement.appendChild(row);
+            });
+
+            // Add the date to the header
+            const headerElement = document.querySelector('.headtext');
+            headerElement.innerText = `Prayer Times for ${today}`;
+
+            // Update the next prayer time
+            displayNextPrayerTime();
+        })
+        .catch(error => console.log(error));
+}
+
+// Function to check prayer times and perform actions
+function checkPrayerTimes() {
+  const delay = 32;
+  const bodyBox = document.getElementById('body-box');
+  const timeElement = document.getElementById('time');
+  const now = new Date();
+  const cells = document.querySelectorAll('.pTime');
+  const currentHour = now.getHours();
+  const currentMinute = now.getMinutes();
+  const futureMinute = (currentMinute + 3) % 60;
+  const cellValues = [];
+
+  cells.forEach(cell => {
+    const cellTime = cell.innerText;
+    const cellHour = parseInt(cellTime.split(':')[0], 10);
+    const cellMinute = parseInt(cellTime.split(':')[1], 10);
+
+    if (currentHour === cellHour && futureMinute === cellMinute) {
+      const player = document.querySelector('.player');
+      bodyBox.style.display = 'block';
+      player.pause();
+
+      setTimeout(() => {
+        player.play();
+        timeElement.style.backgroundColor = '#fff';
+        bodyBox.style.display = 'none';
+      }, 60 * 18 * 1000);
+    }
+
+    cellValues.push(cell.innerText); // Save cell value
+  });
+
+  localStorage.setItem('cellValues', JSON.stringify(cellValues)); // Store cell values in local storage
+}
+
+const TjsonValue = localStorage.getItem('cellValues');
+const timeValues = JSON.parse(TjsonValue);
+
+console.log(timeValues[1])
+
+
+// Function to find the next prayer time
+function findNextPrayerTime() {
+    const now = new Date();
+    const currentTime = now.getTime();
+    const cells = document.querySelectorAll('.pTime');
+    let nextPrayerTime = null;
+    cells.forEach(cell => {
+        const cellTime = cell.innerText;
+        const cellHour = parseInt(cellTime.split(':')[0], 10);
+        const cellMinute = parseInt(cellTime.split(':')[1], 10);
+        const prayerTime = new Date();
+        prayerTime.setHours(cellHour, cellMinute, 0);
+        if (prayerTime.getTime() > currentTime && !nextPrayerTime) {
+            nextPrayerTime = prayerTime;
+        }
+    });
+    return nextPrayerTime;
+}
+
+// Function to display the next prayer time
+function displayNextPrayerTime() {
+    const nextPrayerTime = findNextPrayerTime();
+    const nextPrayerTimeElement = document.getElementById('next-prayer-time');
+    if (nextPrayerTime) {
+        const nextPrayerTimeString = nextPrayerTime.toLocaleTimeString([], { timeStyle: 'short' });
+        nextPrayerTimeElement.innerText = `Next Prayer Time: ${nextPrayerTimeString}`;
+    } else {
+        nextPrayerTimeElement.innerText = 'No upcoming prayer time';
+    }
+}
+
+// Call the fetchPrayerTimes function initially to load the prayer times
+fetchPrayerTimes();
+
+// Call the checkPrayerTimes function every minute to check and perform actions
+setInterval(checkPrayerTimes, 60000);
+
+// Call the fetchPrayerTimes function every day at 12 AM to reload the prayer times
+setInterval(() => {
+    const now = new Date();
+    if (now.getHours() === 0 && now.getMinutes() === 0) {
+        fetchPrayerTimes();
+    }
+}, 60000);
